cb591943cd83a442c65996b684f826e61b89358c
Roman-Lysunetss <rlysunets@gmail.com>
2022-04-08 16:31:23 +0300

    lesson17 (burger added)

---
+++ lesson17/assets/js/script.js
@@ -0,0 +1,12 @@
+
+let burger = document.querySelector(".hamburger");
+
+burger.addEventListener("click",function () {
+   burger.classList.toggle("is-active");
+})
+
+let menu = document.querySelector(".menu-mobile");
+
+burger.addEventListener("click", function() {
+   menu.classList.toggle("active");
+})
