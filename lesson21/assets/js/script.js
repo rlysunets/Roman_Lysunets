2f24912b0ae9cef4ff0b0f2aee5ca049807e7853
Roman-Lysunetss <rlysunets@gmail.com>
2022-04-19 21:25:26 +0300

    lesson21 added burger menu

---
+++ lesson21/assets/js/script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+    $(".hamburger").toggleClass("is-active");
+    $(".menu-mobile").toggleClass("active");
+    $("#over_lay").toggleClass("open");
+}
+
+$(window).on("scroll", function () {
+    if($(window).scrollTop() > 80) {
+        $("#header").addClass("scroll");
+    }
+    else {
+        $("#header").removeClass("scroll");
+    }
+})
+
+$(function() {
+    $(".hamburger, .menu-mobile_link, #over_lay").on("click", function() {
+        toggleMenu();
+    })
+})
