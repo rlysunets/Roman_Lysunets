794c72b0d78fbf2760ba4fb997a68053bf2239dc
Roman-Lysunetss <rlysunets@gmail.com>
2022-06-07 14:24:00 +0300

    lesson36 full

--- JavaScript/lesson36/assets/js/main.js
+++ JavaScript/lesson36/assets/js/main.js
@@ -35,7 +35,7 @@ $(function () {
                   }
             },
             {
-                breakpoint:768,
+                breakpoint:900,
                 settings: {
                     item:3,
                     slideMove:1,
@@ -58,12 +58,53 @@ $(function () {
             }
         ]
     });  
-    $(".new_arrivals_list-prev").click(function(){
+    let slider_partners = $("#partners_lightSlider").lightSlider({
+        item:9,
+        loop:true,
+        slideMove:1,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed: 600,
+        controls: false,
+        slideMargin: 20,
+        responsive: [
+            {
+                breakpoint:992,
+                settings: {
+                    item:7,
+                    slideMove:1,
+                    slideMargin:15,
+                  }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    item:6,
+                    slideMove:1,
+                    slideMargin:15,
+                  }
+            },
+            {
+                breakpoint:560,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove: 2,
+                  }
+            }
+        ]
+    }); 
+    $(".list-prev").click(function(){
         slider.goToPrevSlide(); 
+        slider_partners.goToPrevSlide(); 
     });
-    $(".new_arrivals_list-next").click(function(){
-        slider.goToNextSlide(); 
+    $(".list-next").click(function(){
+        slider.goToNextSlide();
+        slider_partners.goToNextSlide(); 
     });
-
-
 });
